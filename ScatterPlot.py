f8a40503457e27a69e67f2a56c2297901f696ae3
AndrewPerrone1 <perroneap@hotmail.com>
2024-12-10 00:04:19 -0500

    Scatter Plot functioning

--- ScatterPlot.py
+++ ScatterPlot.py
@@ -1,40 +1,27 @@
-import os
-import pandas as pd
-import streamlit as st
-import matplotlib.pyplot as plt
+import os                       #Allows manipulation of file paths
+import pandas as pd             #Data manipulation library
+import streamlit as st          #UI Library
+import matplotlib.pyplot as plt #Plots graphs
 
-subdirectory = "PLCWebScraper"
-file_name = "total_sent.csv"
-file_path = os.path.join(subdirectory, file_name)
+subdirectory = "PLCWebScraper"                      #Directory of csv file
+file_name = "total_sent.csv"                        #Name of csv file
+file_path = os.path.join(subdirectory, file_name)   #Making path to file
+total_sent = pd.read_csv(file_path)                 #Reading file to dataframe
 
-#pos_sentiment = 0                            #pos_sentiment adds all positive sentiments to calculate average
-#neg_sentiment = 0                            #neg_sentiment adds all negatibe sentiments to calculate average
-#neg_sentiment += sent_text["neg"]                                              #Add negative sentiment to aggregate
-#pos_sentiment += sent_text["pos"]                                              #Add positive sentiment to aggregate
-#avg_neg = neg_sentiment / count                                                                     #Calulates average negative sentiment
-#avg_pos = pos_sentiment / count                                                                     #Calculates average positive sentiment
-#avg_sent = "{'avg_neg': " + str(round(avg_neg, 4)) + ", 'avg_pos': " + str(round(avg_pos, 4)) + "}" #Stores the averages in a str
-
-
-data_points = [] 
-with open(file_path, "r") as total_sent:
-    for i, line in enumerate(total_sent):
-        if i % 2 == 1:
-            
-           
-            #print(data_points)
-
-"""
 st.title("Sentiment Analysis")
 
 fig, plot = plt.subplots(figsize=(7, 5))
 
-plot.set_xlim(0, 100)
-plot.set_ylim(0, 100)
+plot.set_xlim(0, total_sent['pos'].max() + 0.01)
+plot.set_ylim(0, total_sent['neg'].max() + 0.01)
 
-plot.set_title('Sentiment Analysis')
+plot.set_title('Market Blogs Sentiments')
 plot.set_xlabel('Positive Sentiment Score')
 plot.set_ylabel('Negative Sentiment Score')
 
+plot.scatter(total_sent['pos'].tolist(), total_sent['neg'].tolist(), s=5)
+avg_pos = total_sent['pos'].mean()
+avg_neg = total_sent['neg'].mean()
+print("avg_pos = " + str(avg_pos) + " avg_neg = " + str(avg_neg))
+
 st.pyplot(fig)
-"""
